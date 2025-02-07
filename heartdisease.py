89a52bef1860798e0e3b0773979a2ed81f68a2f1
Segiraldoa <93294390+Segiraldoa@users.noreply.github.com>
2025-02-07 17:26:29 -0500

    Update heartdisease.py

--- heartdisease.py
+++ heartdisease.py
@@ -270,49 +270,86 @@ if st.sidebar.checkbox("Escalado de datos"):
         st.write(f"Vista previa de los datos escalados usando '{strategy}':")
         st.dataframe(scaled_data.head())
 # Modelo de redes neuronales
-if st.sidebar.checkbox("Utilizar redes Neuronales"):
+if st.sidebar.checkbox("Utilizar redes Neuronales"): 
     st.write("### Redes Neuronales")
     
     st.write("""
     El modelo utilizado consiste en una red neuronal de una capa con 32 neuronas de entrada.
     La base de datos fue codificada con One Hot Encoder y estandarizada con StandardScaler.
     """)
+
+    # Extracción del 
     zip_path = "modelo_entrenado_comprimido.zip"
     extract_path = "modelo_descomprimido"
-    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-        zip_ref.extractall(extract_path)
+    try:
+        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+            zip_ref.extractall(extract_path)
         st.success("Descompresión completada.")
+    except zipfile.BadZipFile:
+        st.error("Error: El archivo ZIP está corrupto o no es un archivo ZIP válido.")
+    except zipfile.LargeZipFile:
+        st.error("Error: El archivo ZIP es demasiado grande y requiere compatibilidad con ZIP64.")
+    except Exception as e:
+        st.error(f"Error durante la descompresión: {str(e)}")
+    
+    st.write("### Indique si desea hacer una predicción de manera manual o usar datos por defecto")
+    selected_column = st.selectbox("Selecciona un método para la predicción", ['Por defecto','Manual'])
+    if selected_column=='Por defecto':
+        st.write("bien")
+        # Buscar el archivo del modelo dentro de la carpeta extraída
+        model_path = None
+        for root, _, files in os.walk(extract_path):
+            for file in files:
+                if file.endswith(".h5"):
+                    model_path = os.path.join(root, file)
+                    break
+                    
+        if model_path:
+            # Cargar el modelo
+            model = tf.keras.models.load_model(model_path)
+            st.success("Modelo cargado correctamente.")
+            X = heartdisease.iloc[:, :-1]
+            y = heartdisease['Cath']
+            X_encoded = pd.get_dummies(X, drop_first=True,dtype= int)
+            scaler = StandardScaler()
+            X_scaled = scaler.fit_transform(X_encoded)
+            label_encoder = LabelEncoder()
+            y_encoded = label_encoder.fit_transform(y)
+            X_train, X_test, y_train, y_test = train_test_split(X_scaled, y_encoded, test_size=0.2, random_state=42)
 
-    # Buscar el archivo del modelo dentro de la carpeta extraída
-    model_path = None
-    for root, _, files in os.walk(extract_path):
-        for file in files:
-            if file.endswith(".h5"):
-                model_path = os.path.join(root, file)
-                break
+            st.write("### Indique los datos por defecto que desea uasr para la predicción")
+            data_model = st.selectbox("Selecciona un método para la predicción", ['Datos 1','Datos 2','Datos 3','Datos 4','Datos 5'])
 
-    if model_path:
-        # Cargar el modelo
-        st.info("Cargando modelo...")
-        model = tf.keras.models.load_model(model_path)
-        st.success("Modelo cargado correctamente.")
-        X = heartdisease.iloc[:, :-1]
-        y = heartdisease['Cath']
-        X_encoded = pd.get_dummies(X, drop_first=True,dtype= int)
-        scaler = StandardScaler()
-        X_scaled = scaler.fit_transform(X_encoded)
-        label_encoder = LabelEncoder()
-        y_encoded = label_encoder.fit_transform(y)
-        X_train, X_test, y_train, y_test = train_test_split(X_scaled, y_encoded, test_size=0.2, random_state=42)
+            if data_model=='Datos 1':
+                input_data = X_train[0].reshape(1, -1)  # Excluir la última columna si es la etiqueta
+                st.write("Datos de entrada:", input_data)
 
-        input_data = X_train[0].reshape(1, -1)  # Excluir la última columna si es la etiqueta
-        st.write("Datos de entrada:", input_data)
+            if data_model=='Datos 2':
+                input_data = X_train[1].reshape(1, -1)  # Excluir la última columna si es la etiqueta
+                st.write("Datos de entrada:", input_data)
 
-        # Realizar predicción
-        prediction = model.predict(input_data)
-        st.write("Predicción del modelo:", prediction)
-    else:
-        st.error("No se encontró un archivo .h5 en el ZIP. Verifica el contenido.")
+            if data_model=='Datos 3':
+                input_data = X_train[2].reshape(1, -1)  # Excluir la última columna si es la etiqueta
+                st.write("Datos de entrada:", input_data)
+
+            if data_model=='Datos 4':
+                input_data = X_train[3].reshape(1, -1)  # Excluir la última columna si es la etiqueta
+                st.write("Datos de entrada:", input_data)
+
+            if data_model=='Datos 5':
+                input_data = X_train[4].reshape(1, -1)  # Excluir la última columna si es la etiqueta
+                st.write("Datos de entrada:", input_data)
+
+            
+            # Realizar predicción
+            prediction = model.predict(input_data)
+            st.write("Predicción del modelo:", prediction)
+        else:
+            st.error("No se encontró un archivo .h5 en el ZIP. Verifica el contenido.")
+        
+        
+    if selected_column=='Manual':
+        st.write("bien2")   
+
     
 
-    
