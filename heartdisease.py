6ed9e180bfe40ce16a1ba7e169233c7573376431
Segiraldoa <93294390+Segiraldoa@users.noreply.github.com>
2025-02-11 03:11:58 -0500

    Update heartdisease.py

--- heartdisease.py
+++ heartdisease.py
@@ -772,110 +772,110 @@ additional_params = {
     'Loss': 0.6126
 }
 
-# # Colocar el checkbox en la barra lateral
-# if st.sidebar.checkbox("Mostrar hiperparámetros del modelo"):
-#     st.write("#### Hiperparámetros del modelo")
+# Colocar el checkbox en la barra lateral
+if st.sidebar.checkbox("Mostrar hiperparámetros del modelo"):
+    st.write("#### Hiperparámetros del modelo")
     
-#     # Mostrar los hiperparámetros del modelo 1 (modelo de clasificación - árbol de decisión)
-#     if hasattr(model1, 'get_params'):
-#         st.write("##### Hiperparámetros del modelo de clasificación (sklearn)")
+    # Mostrar los hiperparámetros del modelo 1 (modelo de clasificación - árbol de decisión)
+    if hasattr(model1, 'get_params'):
+        st.write("##### Hiperparámetros del modelo de clasificación (sklearn)")
         
-#         model1_params = model1.get_params()  # Extraer los hiperparámetros del modelo
+        model1_params = model1.get_params()  # Extraer los hiperparámetros del modelo
         
-#         # Convertir los hiperparámetros a un formato adecuado para una tabla
-#         model1_params_table = [(key, value) for key, value in model1_params.items()] 
+        # Convertir los hiperparámetros a un formato adecuado para una tabla
+        model1_params_table = [(key, value) for key, value in model1_params.items()] 
         
-#         # Limpiar los valores None o <NA> y reemplazarlos con un guion o valor vacío
-#         cleaned_model1_params = [
-#             (key, value if value is not None and value != "<NA>" else "-") 
-#             for key, value in model1_params_table
-#         ]
+        # Limpiar los valores None o <NA> y reemplazarlos con un guion o valor vacío
+        cleaned_model1_params = [
+            (key, value if value is not None and value != "<NA>" else "-") 
+            for key, value in model1_params_table
+        ]
         
-#         # Mostrar los parámetros del modelo 1 como una tabla
-#         model1_params_df = pd.DataFrame(cleaned_model1_params, columns=["Hiperparámetro", "Valor"])
+        # Mostrar los parámetros del modelo 1 como una tabla
+        model1_params_df = pd.DataFrame(cleaned_model1_params, columns=["Hiperparámetro", "Valor"])
         
-#         # Establecer el ancho de las columnas para que se ajusten adecuadamente
-#         model1_params_df.style.set_properties(subset=["Hiperparámetro", "Valor"], width="300px")
+        # Establecer el ancho de las columnas para que se ajusten adecuadamente
+        model1_params_df.style.set_properties(subset=["Hiperparámetro", "Valor"], width="300px")
         
-#         # Mostrar la tabla con estilo
-#         st.dataframe(model1_params_df, use_container_width=True)
+        # Mostrar la tabla con estilo
+        st.dataframe(model1_params_df, use_container_width=True)
         
-#         # Agregar tabla con el Accuracy del modelo de árbol de decisión (con 6 decimales)
-#         st.write("##### Accuracy del modelo de clasificación (Árbol de Decisión)")
-#         accuracy_params = {
-#             "Accuracy": f"{0.836065:.6f}"  # Formatear el Accuracy con 6 decimales
-#         }
-#         accuracy_df = pd.DataFrame(list(accuracy_params.items()), columns=["Métrica", "Valor"])
-#         st.dataframe(accuracy_df, use_container_width=True)
+        # Agregar tabla con el Accuracy del modelo de árbol de decisión (con 6 decimales)
+        st.write("##### Accuracy del modelo de clasificación (Árbol de Decisión)")
+        accuracy_params = {
+            "Accuracy": f"{0.836065:.6f}"  # Formatear el Accuracy con 6 decimales
+        }
+        accuracy_df = pd.DataFrame(list(accuracy_params.items()), columns=["Métrica", "Valor"])
+        st.dataframe(accuracy_df, use_container_width=True)
     
-#     # Mostrar los hiperparámetros del modelo 2 (modelo de red neuronal)
-#     if hasattr(model2, 'get_config'):
-#         st.write("##### Hiperparámetros del modelo de red neuronal (TensorFlow/Keras)")
+    # Mostrar los hiperparámetros del modelo 2 (modelo de red neuronal)
+    if hasattr(model2, 'get_config'):
+        st.write("##### Hiperparámetros del modelo de red neuronal (TensorFlow/Keras)")
         
-#         # Obtener los hiperparámetros de la red neuronal
-#         model2_params = []
-#         for layer in model2.layers:
-#             layer_info = {
-#                 "Capa": layer._class.name_,  # Nombre de la capa (ej. Dense, Conv2D)
-#                 "Hiperparámetros": layer.get_config()  # Obtiene la configuración de la capa
-#             }
-#             model2_params.append(layer_info)
+        # Obtener los hiperparámetros de la red neuronal
+        model2_params = []
+        for layer in model2.layers:
+            layer_info = {
+                "Capa": layer._class.name_,  # Nombre de la capa (ej. Dense, Conv2D)
+                "Hiperparámetros": layer.get_config()  # Obtiene la configuración de la capa
+            }
+            model2_params.append(layer_info)
         
-#         # Crear un diccionario para almacenar los hiperparámetros de cada capa
-#         layers_info = {}
+        # Crear un diccionario para almacenar los hiperparámetros de cada capa
+        layers_info = {}
         
-#         for i, layer in enumerate(model2_params):
-#             layer_name = f"Capa {i+1} ({layer['Capa']})"
-#             layer_config = layer["Hiperparámetros"]
+        for i, layer in enumerate(model2_params):
+            layer_name = f"Capa {i+1} ({layer['Capa']})"
+            layer_config = layer["Hiperparámetros"]
             
-#             for param, value in layer_config.items():
-#                 if param not in layers_info:
-#                     layers_info[param] = []
-#                 layers_info[param].append(value)
+            for param, value in layer_config.items():
+                if param not in layers_info:
+                    layers_info[param] = []
+                layers_info[param].append(value)
 
-#         # Convertir el diccionario en un DataFrame con los hiperparámetros como filas
-#         model2_params_df = pd.DataFrame(layers_info)
+        # Convertir el diccionario en un DataFrame con los hiperparámetros como filas
+        model2_params_df = pd.DataFrame(layers_info)
         
-#         # Transponer la tabla para que las capas estén como columnas y los hiperparámetros como filas
-#         model2_params_df = model2_params_df.transpose()
+        # Transponer la tabla para que las capas estén como columnas y los hiperparámetros como filas
+        model2_params_df = model2_params_df.transpose()
         
-#         # Renombrar las columnas para reflejar el número de capa
-#         model2_params_df.columns = [f"Capa {i+1}" for i in range(len(model2_params))]
+        # Renombrar las columnas para reflejar el número de capa
+        model2_params_df.columns = [f"Capa {i+1}" for i in range(len(model2_params))]
         
-#         # Establecer el ancho de las columnas para que se ajusten adecuadamente
-#         model2_params_df.style.set_properties(subset=model2_params_df.columns, width="300px")
+        # Establecer el ancho de las columnas para que se ajusten adecuadamente
+        model2_params_df.style.set_properties(subset=model2_params_df.columns, width="300px")
         
-#         # Mostrar la tabla con estilo
-#         st.dataframe(model2_params_df, use_container_width=True)
+        # Mostrar la tabla con estilo
+        st.dataframe(model2_params_df, use_container_width=True)
         
-#         # Obtener el learning rate
-#         if hasattr(model2, 'optimizer'):
-#             optimizer = model2.optimizer
-#             if hasattr(optimizer, 'lr'):  # Para versiones más antiguas de Keras
-#                 learning_rate = optimizer.lr.numpy()
-#             elif hasattr(optimizer, 'learning_rate'):  # Para versiones más recientes de TensorFlow
-#                 learning_rate = optimizer.learning_rate.numpy()
+        # Obtener el learning rate
+        if hasattr(model2, 'optimizer'):
+            optimizer = model2.optimizer
+            if hasattr(optimizer, 'lr'):  # Para versiones más antiguas de Keras
+                learning_rate = optimizer.lr.numpy()
+            elif hasattr(optimizer, 'learning_rate'):  # Para versiones más recientes de TensorFlow
+                learning_rate = optimizer.learning_rate.numpy()
                         
-#         # Agregar el learning rate a los parámetros generales
-#         additional_params['Learning Rate'] = learning_rate
+        # Agregar el learning rate a los parámetros generales
+        additional_params['Learning Rate'] = learning_rate
         
-#         # Crear un DataFrame para los parámetros generales
-#         additional_params_df = pd.DataFrame(list(additional_params.items()), columns=["Hiperparámetro", "Valor"])
+        # Crear un DataFrame para los parámetros generales
+        additional_params_df = pd.DataFrame(list(additional_params.items()), columns=["Hiperparámetro", "Valor"])
         
-#         # Ajustar los decimales de los valores para que se muestren con hasta 6 decimales
-#         def format_value(value):
-#             if isinstance(value, (float, int)):
-#                 # Si el valor tiene decimales, mostrarlo con 6 decimales, de lo contrario, mostrarlo como entero
-#                 if value.is_integer():
-#                     return f"{int(value)}"  # Mostrar como entero si no tiene decimales
-#                 return f"{value:.6f}"  # Mostrar con 6 decimales
-#             return value  # Para valores no numéricos, devolver tal cual
+        # Ajustar los decimales de los valores para que se muestren con hasta 6 decimales
+        def format_value(value):
+            if isinstance(value, (float, int)):
+                # Si el valor tiene decimales, mostrarlo con 6 decimales, de lo contrario, mostrarlo como entero
+                if value.is_integer():
+                    return f"{int(value)}"  # Mostrar como entero si no tiene decimales
+                return f"{value:.6f}"  # Mostrar con 6 decimales
+            return value  # Para valores no numéricos, devolver tal cual
         
-#         additional_params_df["Valor"] = additional_params_df["Valor"].apply(format_value)
+        additional_params_df["Valor"] = additional_params_df["Valor"].apply(format_value)
         
-#         # Mostrar la tabla de los parámetros generales
-#         st.write("##### Parámetros Generales del Modelo")
-#         st.dataframe(additional_params_df, use_container_width=True)
+        # Mostrar la tabla de los parámetros generales
+        st.write("##### Parámetros Generales del Modelo")
+        st.dataframe(additional_params_df, use_container_width=True)
     
-#     else:
-#         st.write("El modelo no tiene el método get_config() disponible.")
+    else:
+        st.write("El modelo no tiene el método get_config() disponible.")
